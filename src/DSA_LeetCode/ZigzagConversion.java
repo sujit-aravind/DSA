59cbcbf479960de919fbbb181d3365f3840ac9dd
Sujit Aravind <sujitaravind2003@gmail.com>
2026-03-26 19:43:51 +0530

    newly started

---
+++ src/DSA_LeetCode/ZigzagConversion.java
@@ -0,0 +1,47 @@
+package DSA_LeetCode;
+
+public class ZigzagConversion {
+	public static void main(String[] args) {
+
+		String s = "PAYPALISHIRING";
+
+		System.out.println(ZigzagConversion.convert(s, 3));
+
+	}
+
+	public static String convert(String s, int numRows) {
+
+		if (numRows == 1 || s.length() <= numRows)
+			return s;
+
+		int currentRow = 0;
+		boolean goingDown = true;
+
+		StringBuilder[] row = new StringBuilder[numRows];
+
+		for (int i = 0; i < numRows; i++) {
+			row[i] = new StringBuilder();
+		}
+
+		for (int i = 0; i < s.length(); i++) {
+			row[currentRow].append(s.charAt(i));
+
+			if (currentRow == 0)
+				goingDown = true;
+			if (currentRow == numRows - 1)
+				goingDown = false;
+
+			if (goingDown)
+				currentRow++;
+			else
+				currentRow--;
+		}
+
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < numRows; i++) {
+			result.append(row[i]);
+		}
+
+		return result.toString();
+	}
+}
--- src/Java_Array_Programs/LeftRotateArray.java
+++ src/Java_Array_Programs/LeftRotateArray.java
@@ -22,6 +22,18 @@ public class LeftRotateArray {
 			arr[arr.length - 1] = first;
 		}
 
+		for (int i = 0; i < n; i++) {
+
+			int j, last;
+			last = arr[arr.length - 1];
+
+			for (j = arr.length - 1; j > 0; j--) {
+				arr[j] = arr[j - 1];
+			}
+
+			arr[0] = last;
+		}
+
 		System.out.println();
 		System.out.println("after rotate");
 
