42c950d62a26949830cd0506b4ffbcbbc0cd2a91
Sujit Aravind <sujitaravind2003@gmail.com>
2026-04-04 19:30:17 +0530

    fix done

--- src/Java_Array_Programs/RemoveDuplicateInArray.java
+++ src/Java_Array_Programs/RemoveDuplicateInArray.java
@@ -1,54 +1,28 @@
 package Java_Array_Programs;
-
 public class RemoveDuplicateInArray {
-
-	public static int removeDuplicate(int[] arr) {
-
-		int temp;
-		int index = 1;
-		int total = arr.length - 1;
-
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-
-				if (arr[i] > arr[j]) {
-
-					temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
+	public static int removeDuplicateElements(int arr[], int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		}
+		int[] temp = new int[n];
+		int j = 0;
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[i] != arr[i + 1]) {
+				temp[j++] = arr[i];
 			}
 		}
-
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-
-				if (arr[i] == arr[j]) {
-					temp = arr[j];
-
-					if (j + 1 < total)
-						arr[j] = arr[j + 1];
-					do {
-
-						if (j + index < total) {
-							arr[j + index] = arr[j + index];
-							index++;
-						}
-					} while (arr[j] == total);
-					arr[total] = temp;
-					--total;
-				}
-			}
+		temp[j++] = arr[n - 1];
+		for (int i = 0; i < j; i++) {
+			arr[i] = temp[i];
 		}
-		return total;
+		return j;
 	}
 
 	public static void main(String[] args) {
-
-		int[] arr = { 3, 1, 2, 3, 2, 4, 5, 6 };
-		int total = removeDuplicate(arr);
-		for (int i = 0; i < total; i++) {
+		int arr[] = { 10, 20, 20, 30, 30, 40, 50, 50 };
+		int length = arr.length;
+		length = removeDuplicateElements(arr, length);
+		for (int i = 0; i < length; i++)
 			System.out.print(arr[i] + " ");
-		}
 	}
 }
