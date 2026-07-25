258f14ae788fcd3fc6ad6d1a5309bf631c78ed84
sujit-aravind <sujitaravind2003@gmail.com>
2026-07-25 17:34:13 +0530

    added

--- src/DSA_LeetCode/removeDuplicates.java
+++ src/DSA_LeetCode/removeDuplicates.java
@@ -1,16 +1,48 @@
 package DSA_LeetCode;
 
-public class removeDuplicates {
-//    public int removeDuplicates(int[] nums) {
-//    	//nums : 1 1 2 2 3 3 4
-//    	
-//    	int length = nums.length;
-//    	int[] duplicate = new int[length];
-//    	
-//    	for (int i = 0; i < nums.length; i++) {
-//    		
-////    		if(nums[i] == duplicate[i])
-//    		
-//		}
-//    }
+class removeDuplicates {
+	public int removeDuplicates(int[] nums) {
+
+		String index;
+		int position = 0;
+		boolean status;
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (i == 0) {
+				index = String.valueOf(i);
+				position = nums[i];
+			}
+
+			if (i != 0) {
+
+				if (position == nums[i]) {
+
+				}
+
+			}
+		}
+		return 0;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+		int[] nums = { 1, 1, 2, 2, 3, 3, 4 };
+
+		removeDuplicates remove = new removeDuplicates();
+		int k = remove.removeDuplicates(nums);
+		System.out.println("k = " + k);
+		System.out.print("Array: ");
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + " ");
+		}
+	}
 }
